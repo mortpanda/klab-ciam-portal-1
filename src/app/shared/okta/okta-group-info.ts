baddb0b2724bd68b4f3c2398b16c469f37d02bef
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-01-14 16:22:24 +0900

    main
    main

--- src/app/shared/okta/okta-group-info.ts
+++ src/app/shared/okta/okta-group-info.ts
@@ -1,6 +1,6 @@
-  //ANGULAR-CIAM-DEMO-PORTAL-CALCULATOR
-  //ANGULAR-CIAM-DEMO-PORTAL-NEWS
-  //ANGULAR-CIAM-DEMO-PORTAL-PROFILE
+//ANGULAR-CIAM-DEMO-PORTAL-CALCULATOR
+//ANGULAR-CIAM-DEMO-PORTAL-NEWS
+//ANGULAR-CIAM-DEMO-PORTAL-PROFILE
 export interface OktaGroupInfo {
   name: string;
   groupname: string;
@@ -13,18 +13,18 @@ export const PortalApps = [
     name: 'Profile Viewer',
     groupname: 'ANGULAR-CIAM-DEMO-PORTAL-PROFILE',
     img: 'assets/img/okta_user.png',
-url:'https://mortpanda.github.io/jpn-corp-ae-portal-profile/',
+    url: 'https://mortpanda.github.io/klab-ciam-portal-profile/',
   },
   {
     name: 'News App',
     groupname: 'ANGULAR-CIAM-DEMO-PORTAL-NEWS',
-    img:'assets/img/okta_annoucement.png',
-    url:'https://mortpanda.github.io/jpn-corp-ae-portal-news/',
+    img: 'assets/img/okta_annoucement.png',
+    url: 'https://mortpanda.github.io/klab-ciam-portal-news/',
   },
   {
     name: 'DENTAKU',
     groupname: 'ANGULAR-CIAM-DEMO-PORTAL-CALCULATOR',
-    img:'assets/img/okta_scalable_apps.png',
-    url:'https://mortpanda.github.io/jpn-corp-ae-portal-calc/',
+    img: 'assets/img/okta_scalable_apps.png',
+    url: 'https://mortpanda.github.io/klab-ciam-portal-calc/home/',
   },
 ]
